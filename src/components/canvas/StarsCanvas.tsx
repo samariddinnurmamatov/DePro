19aef0544a8dc9d0c2a5f4f805fbbf2cb8c19dd4
Samariddin Nurmamatov <samariddinnurmamatov111@gmail.com>
2024-09-15 21:20:47 +0500

    contact

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -1,63 +1,3 @@
-// "use client";
-
-// import React, { useState, useRef, Suspense } from "react";
-// import { Points, PointMaterial } from "@react-three/drei";
-// import { Canvas, useFrame } from "@react-three/fiber";
-// // @ts-ignore
-// import { Group } from "three"; 
-
-// const StarBackground = (props: any) => {
-//   const ref = useRef<Group>(null); // Correctly type the ref
-
-//   // Simplified sphere generation
-//   const [sphere] = useState(() => {
-//     const positions = [];
-//     for (let i = 0; i < 5000; i++) {
-//       const theta = Math.random() * Math.PI * 2;
-//       const phi = Math.acos(Math.random() * 5 - 1);
-//       const x = Math.sin(phi) * Math.cos(theta);
-//       const y = Math.sin(phi) * Math.sin(theta);
-//       const z = Math.cos(phi);
-//       positions.push(x, y, z);
-//     }
-//     return new Float32Array(positions);
-//   });
-
-//   useFrame((state, delta) => {
-//     if (ref.current) {
-//       ref.current.rotation.x -= delta / 10;
-//       ref.current.rotation.y -= delta / 15;
-//     }
-//   });
-
-//   return (
-//     <group rotation={[0, 0, Math.PI / 4]}>
-//       <Points ref={ref} positions={sphere} stride={4} frustumCulled {...props}>
-//         <PointMaterial
-//           transparent
-//           color="#f272c8"
-//           size={0.005}
-//           sizeAttenuation={true}
-//           depthWrite={false}
-//         />
-//       </Points>
-//     </group>
-//   );
-// };
-
-// const StarsCanvas = () => (
-//   <div className="absolute w-full h-full inset-0 z-[-1]">
-//     <Canvas camera={{ position: [0, 0, 1] }}>
-//       <Suspense fallback={null}>
-//         <StarBackground />
-//       </Suspense>
-//     </Canvas>
-//   </div>
-// );
-
-// export default StarsCanvas;
-
-
 "use client";
 
 import React, { useState, useRef, Suspense } from "react";
@@ -73,8 +13,8 @@ const StarBackground = (props: any) => {
   const [sphere] = useState(() => {
     const positions = [];
     for (let i = 0; i < 5000; i++) {
-      const theta = Math.random() * 3 * Math.PI; // 0 to 2*PI for full rotation
-      const phi = Math.acos(9 * Math.random() - 1); // correct range for phi
+      const theta = Math.random() * 2 * Math.PI; // 0 to 2*PI for full rotation
+      const phi = Math.acos(2 * Math.random() - 1); // correct range for phi
 
       const x = Math.sin(phi) * Math.cos(theta);
       const y = Math.sin(phi) * Math.sin(theta);
