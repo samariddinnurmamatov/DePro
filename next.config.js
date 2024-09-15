4a4eb33fe9f273daa2a01884657b645feac48d14
Samariddin Nurmamatov <samariddinnurmamatov111@gmail.com>
2024-09-15 15:07:47 +0500

    contact

--- next.config.js
+++ next.config.js
@@ -2,23 +2,10 @@
 const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
-  productionBrowserSourceMaps: true,
+  productionBrowserSourceMaps: false,
   reactStrictMode: false,
   swcMinify: true,
   i18n,
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // barcha fayllar uchun keshlashni o'rnating
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable', // 1 yilga mo'ljallangan kesh
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
