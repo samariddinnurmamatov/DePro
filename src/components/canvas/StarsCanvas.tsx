cfcf6bf0382a85a71206343bb6f10f5cf2d8d018
Samariddin Nurmamatov <samariddinnurmamatov111@gmail.com>
2024-09-15 21:05:49 +0500

    contact

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -4,25 +4,23 @@ import React, { useState, useRef, Suspense } from "react";
 import { Points, PointMaterial } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 // @ts-ignore
-import * as THREE from "three"; // three.js import
+import { Group } from "three"; 
 
 const StarBackground = (props: any) => {
-  const ref = useRef<THREE.Group>(null);
+  const ref = useRef<Group>(null); // Correctly type the ref
 
-  // Spheres positions generation
+  // Simplified sphere generation
   const [sphere] = useState(() => {
     const positions = [];
     for (let i = 0; i < 5000; i++) {
-      const theta = Math.random() * 2 * Math.PI; // 0 to 2*PI for full rotation
-      const phi = Math.acos(2 * Math.random() - 1); // correct range for phi
-
+      const theta = Math.random() * Math.PI * 2;
+      const phi = Math.acos(Math.random() * 5 - 1);
       const x = Math.sin(phi) * Math.cos(theta);
       const y = Math.sin(phi) * Math.sin(theta);
       const z = Math.cos(phi);
-
       positions.push(x, y, z);
     }
-    return new Float32Array(positions); // Return positions as Float32Array
+    return new Float32Array(positions);
   });
 
   useFrame((state, delta) => {
@@ -33,8 +31,8 @@ const StarBackground = (props: any) => {
   });
 
   return (
-    <group rotation={[0, 0, Math.PI / 4]} ref={ref}>
-      <Points positions={sphere} stride={3} frustumCulled {...props}>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={4} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
