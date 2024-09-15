f729f776b76458efd3b55c40198602f168773625
Samariddin Nurmamatov <samariddinnurmamatov111@gmail.com>
2024-09-15 21:41:35 +0500

    contact

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -13,15 +13,18 @@ const StarBackground = (props: any) => {
   const [sphere] = useState(() => {
     const positions = [];
     for (let i = 0; i < 5000; i++) {
-      const theta = Math.random() * 2 * Math.PI; // 0 to 2*PI for full rotation
-      const phi = Math.acos(2 * Math.random() - 1); // correct range for phi
-
-      const x = Math.sin(phi) * Math.cos(theta);
-      const y = Math.sin(phi) * Math.sin(theta);
-      const z = Math.cos(phi);
-
+      const theta = Math.random() * Math.PI * 2; // Full circle for theta
+      const phi = Math.acos(Math.random() * 2 - 1); // Correct range for phi, [-1, 1]
+    
+      const r = Math.random() * 4 + 1; // Scale radius between 1 and 5
+    
+      const x = r * Math.sin(phi) * Math.cos(theta);
+      const y = r * Math.sin(phi) * Math.sin(theta);
+      const z = r * Math.cos(phi);
+    
       positions.push(x, y, z);
     }
+    
     return new Float32Array(positions); // Return positions as Float32Array
   });
 
